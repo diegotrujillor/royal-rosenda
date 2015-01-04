1955d371f3e3f7b3cb36859e17bb0461c76a250d
diegotrujillor <diegotrujillor@gmail.com>
2015-01-04 04:33:56 -0500

    Update whole app
    New functionalities, autocomplete, bootstrap, ajax requests, others.

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ royal-rosenda
 
 This is an agile web-app development test for TMDb.org API proposed by Alert Logic.
 
--Used Frameworks-
+-Used Frameworks and Technologies-
 =================
-Frontend: Angular.js, jQuery.
-Backend: TMDb Public API.
+Frontend: Angular.js, jQuery, jQuery UI, Bootstrap for responsive design, CSS3, HTML5.
+Backend: Ajax, TMDb Public API. (discover and search services)
 Pattern: MVC.
 
 Without the provided TMDb API, the architecture was oriented to an end-to-end application based on MEAN fullstack.
--- main.js
+++ main.js
@@ -1,3 +1,51 @@
-angular.module('ngAlertLogicApp', []).controller('mainController', function($scope){
-  $scope.person = 'hello test';
-});
+/*TMDb API used service: /search/person */
+/*****************************************/
+/*Date: 3/JAN/2014*/
+/*Author: <a href="mailto:diegotrujillor@gmail.com">Diego Fernando Trujillo</a>*/
+/*Description: This function allows asynchronously autocomplete actors while type names, i.e: Bill M*/
+/****************************************************************************************************/
+function getPersons(){
+	$(function() {
+		var availableTags = [];
+		
+		if ($("#persons").val()){		
+			$.ajax({
+				url: 'http://api.themoviedb.org/3/search/person?&query=' + $("#persons").val().replace(/\s+/g,'%20') + '&api_key=f660eb4c7b379f980417e33f5054a807',
+				dataType: 'json',
+				success: function(data, status){
+					$.each(data.results, function(key, value){
+						availableTags.push(value.name);
+					});
+				},
+				error: function(e){
+					console.log('Error: ' + e);
+				}
+			});
+		}
+		$("#persons").autocomplete({
+			source: availableTags
+		});
+	});
+}
+
+/*TMDb API used service: /discover/movie */
+/*****************************************/
+/*Date: 4/JAN/2014*/
+/*Author: <a href="mailto:diegotrujillor@gmail.com">Diego Fernando Trujillo</a>*/
+/*Description: This module allows fetch the movies data given an actor. (previously selected)*/
+/*********************************************************************************************/
+(function() {
+	angular.module('ngAlertLogicApp', []).controller('mainController', function($scope, $http) {
+		$scope.searchPerson = function(){
+			//if($scope.person){
+				$http.get('http://api.themoviedb.org/3/discover/movie?with_cast=1532&sort_by=release_date.desc&api_key=f660eb4c7b379f980417e33f5054a807')
+					.success(function(data){
+						$scope.persons = data.results;
+					})
+					.error(function(data){
+						console.log('Error: ' + data)
+					});	
+			//}
+		};
+	});
+}());
