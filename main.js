79f908dc92ff8c3561ec6ebfa05ae87cf7e9acc5
diegotrujillor <diegotrujillor@gmail.com>
2015-01-04 17:39:17 -0500

    Search movies by person implemented.

--- main.js
+++ main.js
@@ -5,26 +5,26 @@
 /*Description: This function allows asynchronously autocomplete actors while type names, i.e: Bill M*/
 /****************************************************************************************************/
 function getPersons(){
-	$(function() {
-		var availableTags = [];
-		
-		if ($("#persons").val()){		
+	$("#persons").autocomplete({
+		source: function(request, response){
 			$.ajax({
 				url: 'http://api.themoviedb.org/3/search/person?&query=' + $("#persons").val().replace(/\s+/g,'%20') + '&api_key=f660eb4c7b379f980417e33f5054a807',
 				dataType: 'json',
+				data: request,
 				success: function(data, status){
-					$.each(data.results, function(key, value){
-						availableTags.push(value.name);
-					});
+					response($.map(data.results, function(item){
+						return { value:item.name, storeid:item.id };
+					}))
 				},
 				error: function(e){
 					console.log('Error: ' + e);
 				}
 			});
+		},
+		minLength: 2,
+		select: function(event, ui){
+			$('#persons').attr("data-id", ui.item.storeid);
 		}
-		$("#persons").autocomplete({
-			source: availableTags
-		});
 	});
 }
 
@@ -37,15 +37,15 @@ function getPersons(){
 (function() {
 	angular.module('ngAlertLogicApp', []).controller('mainController', function($scope, $http) {
 		$scope.searchPerson = function(){
-			//if($scope.person){
-				$http.get('http://api.themoviedb.org/3/discover/movie?with_cast=1532&sort_by=release_date.desc&api_key=f660eb4c7b379f980417e33f5054a807')
+			if($('#persons').attr("data-id")){
+				$http.get('http://api.themoviedb.org/3/discover/movie?with_cast=' + $('#persons').attr("data-id") + '&sort_by=release_date.desc&api_key=f660eb4c7b379f980417e33f5054a807')
 					.success(function(data){
-						$scope.persons = data.results;
+							$scope.persons = data.results;
 					})
 					.error(function(data){
 						console.log('Error: ' + data)
 					});	
-			//}
+			}
 		};
 	});
 }());
